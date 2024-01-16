8bac77adcc651512707119492ab5b96791a8a9b9
lucadisaba <lucadisabatino97@gmail.com>
2024-01-16 11:58:30 +0100

    Home message changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fauni-frontend';
+  title = 'fauni-frontend super ready!!!';
 }
